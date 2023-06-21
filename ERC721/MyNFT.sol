a17a5f013852f69b1ac388f6c1483e036fb1856f
tenghao <tenghao@hundwe.com>
2023-06-21 11:47:14 +0800

    一个简单的中文注释的ERC-721代币合约(nft)

--- ERC721/MyNFT.sol
+++ ERC721/MyNFT.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
-
+// 一个简单的中文注释的ERC-721代币合约(nft)
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
